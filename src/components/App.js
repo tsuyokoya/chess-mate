679cb0dd2d04795866533ffd16d15ba21be55174
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-22 15:41:42 -0400

    Imported components and added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Room from "./Room";
 
 const App = () => {
-  return "App";
+  return (
+    <div className="bg-black h-screen text-white">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
