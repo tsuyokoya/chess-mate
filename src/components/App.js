770de70bca812f3b1f682a3f59fd0d3c726cf6c7
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-22 18:29:57 -0400

    Added Navbar component and created routes for login and signup

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Room from "./Room";
+import Login from "./Login";
+import Signup from "./Signup";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div className="bg-black h-screen text-white">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/room" element={<Room />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
