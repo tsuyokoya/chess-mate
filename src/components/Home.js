b1c6e7aca5d49ef8e805a7194582e7a237bd4a0d
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-22 18:31:39 -0400

    Added header, Play as Guest button, and general styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,26 @@ import Button from "./Button";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <header>
-        <h1 className="text-3xl font-bold underline">
-          <FontAwesomeIcon icon={faChess} />
+        <h1 className="text-7xl text-lightGreen mt-3 p-3 font-bold underline text-center">
+          <FontAwesomeIcon
+            icon={faChess}
+            className="animate-bounce temporary-bounce mr-2"
+          />
           Chess Mate
         </h1>
-        <p>Play chess with your favorite mate!</p>
+        <p className="text-3xl text-center">
+          Play <span className="text-lightGreen">chess</span> with your favorite{" "}
+          <span className="text-lightGreen">mate</span>!
+        </p>
       </header>
-      <Button icon={faChess} text="Play as Guest" path="/room" />
+      <Button
+        icon={faChess}
+        text="Play as Guest"
+        path="/room"
+        textSize="text-5xl"
+      />
     </div>
   );
 };
