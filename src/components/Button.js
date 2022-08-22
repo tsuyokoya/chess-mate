89c0a361d53787e26288047da2ae31fb00de2af5
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-22 18:31:04 -0400

    Added styling, added textSize prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChess } from "@fortawesome/free-solid-svg-icons";
 
-const Button = ({ icon, text, path }) => {
+const Button = ({ icon, text, path, textSize = "text-xl" }) => {
   return (
-    <div>
+    <div
+      className={
+        "bg-lightGreen text-center block p-2 w-1/2 border-none rounded-lg hover:bg-green-700 hover:cursor-pointer " +
+        textSize
+      }
+    >
       <Link to={path}>
-        <FontAwesomeIcon icon={icon} /> {text}
+        {icon ? <FontAwesomeIcon icon={icon} /> : null} {text}
       </Link>
     </div>
   );
