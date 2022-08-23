93919f4461406e38bff69ba94e318cc32a6be0f8
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-23 14:32:24 -0400

    Updated inputs to controlled components and added css styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,43 @@
-import React from "react";
-import Button from "./Button";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(() => ({ ...formData, [name]: value }));
+  };
+  console.log(formData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    for (const value of Object.values(formData)) {
+      if (value === "") {
+        alert("Please enter your username and password");
+        return;
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-4xl m-5">Have an Account? Log in Below!</h2>
-      <form className="text-3xl flex flex-col items-center w-full">
+      <form
+        className="text-3xl flex flex-col items-center w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="flex justify-center w-full mb-3">
           <label htmlFor="username" hidden>
             Username
           </label>
           <input
-            className="block p-2 w-1/2 border-none rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
+            className="block p-2 w-1/2 border focus:border focus:border-lightGreen focus:ring-2 focus:ring-lightGreen rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
             type="text"
             placeholder="Username"
+            value={formData.username}
+            onChange={handleInputChange}
             id="username"
+            name="username"
           />
         </div>
         <div className="flex justify-center w-full mb-3">
@@ -22,13 +45,18 @@ const Login = () => {
             Password
           </label>
           <input
-            className="block p-2 w-1/2 border-none rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
+            className="block p-2 w-1/2 border focus:border focus:border-lightGreen focus:ring-2 focus:ring-lightGreen rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
             type="text"
             placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
             id="password"
+            name="password"
           />
         </div>
-        <Button text="Login" path="/" textSize="text-3xl" />
+        <button className="w-1/2 p-2 rounded-lg bg-lightGreen text-3xl hover:bg-green-600">
+          Login
+        </button>
       </form>
     </div>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,20 +1,48 @@
-import React from "react";
-import Button from "./Button";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(() => ({ ...formData, [name]: value }));
+  };
+  console.log(formData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("SUBMITTED");
+    for (const value of Object.values(formData)) {
+      if (value === "") {
+        alert("Please enter all fields");
+        return;
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-4xl m-5">Sign up for a New Account!</h2>
-      <form className="text-3xl flex flex-col items-center w-full">
+      <form
+        className="text-3xl flex flex-col items-center w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="flex justify-center w-full mb-3">
           <label htmlFor="username" hidden>
             Username
           </label>
           <input
-            className="block p-2 w-1/2 border-none rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
+            className="block p-2 w-1/2 border focus:border focus:border-lightGreen focus:ring-2 focus:ring-lightGreen rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
             type="text"
             placeholder="Username"
+            value={formData.username}
+            onChange={handleInputChange}
             id="username"
+            name="username"
           />
         </div>
         <div className="flex justify-center w-full mb-3">
@@ -22,10 +50,13 @@ const Signup = () => {
             Password
           </label>
           <input
-            className="block p-2 w-1/2 border-none rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
+            className="block p-2 w-1/2 border focus:border focus:border-lightGreen focus:ring-2 focus:ring-lightGreen rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
             type="text"
             placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
             id="password"
+            name="password"
           />
         </div>
         <div className="flex justify-center w-full mb-3">
@@ -33,13 +64,18 @@ const Signup = () => {
             Confirm Password
           </label>
           <input
-            className="block p-2 w-1/2 border-none rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
+            className="block p-2 w-1/2 border focus:border focus:border-lightGreen focus:ring-2 focus:ring-lightGreen rounded-lg dark:bg-gray-700 dark:placeholder-gray-300 dark:text-white"
             type="text"
             placeholder="Confirm Password"
+            value={formData.passwordConfirm}
+            onChange={handleInputChange}
             id="password-confirm"
+            name="passwordConfirm"
           />
         </div>
-        <Button text="Login" path="/" textSize="text-3xl" />
+        <button className="w-1/2 p-2 rounded-lg bg-lightGreen text-3xl hover:bg-green-600">
+          Signup
+        </button>
       </form>
     </div>
   );
