096f822c448853dd6faabb8214cf0f02e3aa2767
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-31 13:20:37 -0400

    Update tile size

--- src/components/Board/Tile.js
+++ src/components/Board/Tile.js
@@ -3,22 +3,22 @@ import React from "react";
 const Tile = ({ num, image }) => {
   if (num % 2 === 0) {
     return (
-      <div className="Board-tile h-[100px] w-[100px] bg-lightGreen">
+      <div className="Board-tile h-[85px] w-[85px] bg-lightGreen">
         {image ? (
           <div
             style={{ backgroundImage: `url(${image})` }}
-            className="Board-piece h-[100px] w-[100px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
+            className="Board-piece h-[85px] w-[85px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
           ></div>
         ) : null}
       </div>
     );
   } else {
     return (
-      <div className="Board-tile h-[100px] w-[100px] bg-cream">
+      <div className="Board-tile h-[85px] w-[85px] bg-cream">
         {image ? (
           <div
             style={{ backgroundImage: `url(${image})` }}
-            className="Board-piece h-[100px] w-[100px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
+            className="Board-piece h-[85px] w-[85px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
           ></div>
         ) : null}
       </div>
