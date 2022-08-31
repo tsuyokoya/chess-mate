011e6b722bfb9eada428c36402d05497e81fcae6
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-31 11:59:01 -0400

    Update to render a div with background image for pieces instead of image; update cursor on hover and mouse press

--- src/components/Board/Tile.js
+++ src/components/Board/Tile.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-const Tile = ({ num }) => {
+const Tile = ({ num, image }) => {
   if (num % 2 === 0) {
-    return <div className="Board-tile h-[100px] w-[100px] bg-lightGreen"></div>;
+    return (
+      <div className="Board-tile h-[100px] w-[100px] bg-lightGreen">
+        {image ? (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="Board-piece h-[100px] w-[100px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
+          ></div>
+        ) : null}
+      </div>
+    );
   } else {
-    return <div className="Board-tile h-[100px] w-[100px] bg-cream"></div>;
+    return (
+      <div className="Board-tile h-[100px] w-[100px] bg-cream">
+        {image ? (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="Board-piece h-[100px] w-[100px] bg-no-repeat bg-center bg-cover cursor-grab active:cursor-grabbing"
+          ></div>
+        ) : null}
+      </div>
+    );
   }
 };
 
