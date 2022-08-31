45995ed8a5bedcb4f92b231c217a2b4cbf23fe1e
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-08-31 10:56:23 -0400

    Update to use Tile component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Tile from "./Tile";
+
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
@@ -7,24 +9,9 @@ const Board = () => {
   let board = [];
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
-      const key = horizontalAxis[i] + verticalAxis[j];
+      const id = horizontalAxis[i] + verticalAxis[j];
       const num = i + j + 2;
-
-      if (num % 2 === 0) {
-        board.push(
-          <div
-            key={key}
-            className="Board-tile h-[100px] w-[100px] bg-lightGreen"
-          ></div>
-        );
-      } else {
-        board.push(
-          <div
-            key={key}
-            className="Board-tile h-[100px] w-[100px] bg-cream"
-          ></div>
-        );
-      }
+      board.push(<Tile num={num} key={id} />);
     }
   }
   return (
